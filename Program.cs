108dec3e3871b10c2068bb02738e7c72d83b7491
Chris Burgdorff <wesborland1234@aol.com>
2022-09-07 13:18:20 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Confluent.Kafka;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Waiting for messages from Kafka!");
+
+var kafkaConfig = new ConsumerConfig
+{ 
+    BootstrapServers = "localhost:9092",
+    GroupId = "MyGroup",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+CancellationTokenSource cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) => {
+    e.Cancel = true;
+    cts.Cancel();
+};
+
+using (IConsumer<Ignore, string> consumer = new ConsumerBuilder<Ignore, string>(kafkaConfig).Build())
+{
+    consumer.Subscribe("MyTopic");
+    try
+    {
+        while(true)
+        {
+            var response = consumer.Consume(cts.Token);
+            if (response != null)
+            {
+                Console.WriteLine("Message from Kafka: " + response.Message.Value);
+            }
+            else
+            {
+                Console.WriteLine("Resposne is null");
+            }
+        }
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
